3ab615ca19770643c79fbcd8032f63f926c08108
bhavesh <bhavesh.suthar@darwinpgc.com>
2023-02-03 11:01:47 +0530

    create question complete

--- src/components/QuesInput.jsx
+++ src/components/QuesInput.jsx
@@ -5,21 +5,23 @@ import Input from './Input'
 
 
 
-const QuesInput = ({ add, remove, data, setData, index }) => {
+const QuesInput = ({ remove, data, setData, index }) => {
 
-    const changeValue = () => {
-        setData([...data,
-        {
+    const add = () => {
+        setData([...data, {
             question: "",
-            optionA: "",
-            optionB: "",
-            optionC: "",
-            optionD: "",
-            answer: 0,
-        }
-        ])
+            option: new Array(4).fill(""),
+            answer: "",
+        }])
+    }
 
-        add()
+    const insertValue = (event, column, i = null) => {
+        setData(
+            [...data.map((item, key) => key == index
+                ? column == "option"
+                    ? { ...item, [column]: [...item[column].map((single, key) => key == i ? single = event.target.value : single)] }
+                    : { ...item, [column]: event.target.value }
+                : item)])
     }
 
     return (
@@ -28,54 +30,32 @@ const QuesInput = ({ add, remove, data, setData, index }) => {
                 <div>
                     <Input
                         value={data[index]?.question}
-                        onChange={(e) => (
-                            setData([...data.map((item, key) => key == index ? { ...item, question: e.target.value } : item)])
-                        )}
+                        onChange={(event) => insertValue(event, 'question')}
                         placeholder={"enter your question ....."}
                     />
                 </div>
                 <br></br>
                 <div className="row">
+                    {data[index]?.option.map((item, i) => (
+                        <div key={i} className="col-2">
+                            <Input
+                                value={item}
+                                onChange={(event) => insertValue(event, "option", i)}
+                                placeholder={`option ${i + 1}`}
+                            />
+                        </div>
+                    ))}
                     <div className="col-2">
-                        <Input
-                            value={data[index]?.optionA}
-                            onChange={(e) => {
-                                setData([...data.map((item, key) => key == index ? { ...item, optionA: e.target.value } : item)])
-                            }}
-
-                            placeholder={"Option A"}
-                        />
+                        <select className="form-select" onChange={(event) => insertValue(event, "answer")} value={data[index]?.answer}>
+                            <option value={""}>Select answer</option>
+                            {
+                                data[index].option.map((item, key) => 
+                                (<option key={key} value={key + 1}>{item}</option>))
+                            }
+                        </select>
                     </div>
-                    {/* <div className="col-2">
-                        <Input
-                            value={data.options}
-                            onChange={(e) => setData(data => ({ ...data[0], optionB: e.target.value }))}
-                            placeholder={"Option B"}
-                        />
-                    </div>
-                    <div className="col-2">
-                        <Input
-                            value={data.options}
-                            onChange={(e) => setData(data => ({ ...data[0], optionC: e.target.value }))}
-                            placeholder={"Option C"}
-                        />
-                    </div>
-                    <div className="col-2">
-                        <Input
-                            value={data.options}
-                            onChange={(e) => setData(data => ({ ...data[0], optionD: e.target.value }))}
-                            placeholder={"Option D"}
-                        />
-                    </div>
-                    <div className="col-2">
-                        <Input
-                            value={data.answer}
-                            onChange={(e) => setData(data => ({ ...data[0], answer: e.target.value }))}
-                            placeholder={"Answer"}
-                        />
-                    </div> */}
                     <div className="col-2 align-self-end ">
-                        <button className="btn btn-primary" onClick={changeValue}>Add More</button>
+                        <button className="btn btn-primary me-2" onClick={add}>Add More</button>
                         <button className="btn btn-primary" onClick={remove}>Remove</button>
                     </div>
                 </div>
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,49 +1,64 @@
 import '../assets/pages.css'
+
 import { useRef, useState } from 'react'
 import Navbar from '../components/Navbar'
 import { Button } from '../components/Button'
 import QuesInput from '../components/QuesInput'
+import axios from 'axios'
 
 
 function App() {
   const [count, setCount] = useState([0]);
+  const [uniqueCode, setUniqueCode] = useState();
 
   const [data, setData] = useState([{
     question: "",
-    optionA: "",
-    optionB: "",
-    optionC: "",
-    optionD: "",
-    answer: 0,
+    option: new Array(4).fill(""),
+    answer: "",
   }])
 
   function remove(key) {
-
     setData(data.filter((item, index) => index != key))
-    setCount(count.filter((item, index) => index != key))
     console.log(data)
   }
 
+  function sendData() {
+    setData(data.filter((item, index) => item.question != ""))
 
+    const formData = new FormData();
+    formData.append("data", JSON.stringify(data))
+
+    axios.post('http://localhost:9000/question/create', formData, {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+      }
+    })
+      .then(res => {
+        console.log(res)
+        if (res.status == 200) {
+          location.replace(res.data.data.link)
+        }
+      })
+      .catch(err => console.log(err))
+  }
 
   return (
     <div className='parent-container'>
       <Navbar />
       <div className="child-container">
         {
-          count.map((item, key) => (
+          data.map((item, key) => (
             <div key={key}>
               <QuesInput
                 index={key}
                 data={data}
                 setData={setData}
                 remove={() => remove(key)}
-                add={() => setCount([...count, count.length])}
               />
             </div>
           ))
         }
-        <button onClick={() => console.log(data)}>send</button>
+        <button onClick={() => sendData()}>send</button>
       </div>
     </div>
   )
